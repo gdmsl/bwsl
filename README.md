934d1316586e8a4092da1963eefb238b75bffb6a
Guido Masella <guido.masella@gmail.com>
2017-07-13 10:48:39 +0200

    Dump the status of a marsenne twister

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BeagleWarlord's Support Library
+# BeagleWarlord's Support Library and Tools
 
 This is an header-only support library in C++ which provides some utilities
 to write less but better code in modern C++ ( > 11).
--- applications/dumpmt.cpp
+++ applications/dumpmt.cpp
@@ -1,4 +1,4 @@
-//===-- Foo.cpp ------------------------------------------------*- C++ -*-===//
+//===-- dumpmt.cpp ------------------------------------------------*- C++ -*-===//
 //
 //                       BeagleWarlord's Support Library
 //
@@ -11,11 +11,29 @@
 /// \version    0.1
 /// \author     Guido Masella (guido.masella@gmail.com)
 /// \date       May, 2017
-/// \brief      Implementation of the Foo Class
+/// \brief      Dump the status of a fresh initialized marsenne twister
 ///
 //===---------------------------------------------------------------------===//
-#include <Foo.hpp>
 
-using namespace bwsl;
+// std
+#include <fstream>
+#include <iostream>
+#include <random>
+
+int
+main (int ac, char **av)
+{
+  if (ac < 2) {
+    return EXIT_FAILURE;
+  }
+
+  auto seed = static_cast<unsigned long>(atoi(av[1]));
+
+  auto rng = std::mt19937_64{seed};
+
+  std::cout << rng << std::endl;
+
+  return EXIT_SUCCESS;
+}
 
 // vim: set ft=cpp ts=2 sts=2 et sw=2 tw=80: //
